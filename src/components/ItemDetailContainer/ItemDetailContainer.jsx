87cd9f540fa81847221cfc56469f8fd9bf884b8a
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-09-21 17:44:18 -0300

    ok item detail doc & getDoc

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -2,38 +2,35 @@ import React, { useState } from "react";
 
 import ItemDetail from "../ItemDetail/ItemDetail";
 import Loader from "../Loader/Loader";
-import { products } from "../../mock/product";
 import s from "./itemDetailContainer.module.css";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { collection, getDocs } from "firebase/firestore";
+import { doc, getDoc, collection } from "firebase/firestore";
 import { db } from "../../firebaseConfig";
 export default function ItemDetailContainer() {
   const { id } = useParams();
   const idParsed = id;
   //eslint-disable-next-line
   const [item, setItem] = useState({});
-  const [items, setItems] = useState([]);
+
   useEffect(() => {
     const itemCollection = collection(db, "productos");
-    getDocs(itemCollection)
-      .then((querySnapshot) => {
-        const productos = querySnapshot.docs.map((doc) => {
-          return { id: doc.id, ...doc.data() };
-        });
-        setItems(productos);
+    const ref = doc(itemCollection, idParsed);
+    getDoc(ref)
+      .then((res) => {
+        setItem({ id: res.id, ...res.data() });
       })
       .catch((error) => {
         console.log("Error searching items", error);
       });
-
-    const itemFound = items.find((prod) => prod.id === idParsed);
-    setItem(itemFound);
-  }, [idParsed, items]);
+    return () => {
+      setItem({});
+    };
+  }, [idParsed]);
 
   return (
     <div className={s.itemDetailContainer}>
-      {item ? <ItemDetail item={item} /> : <Loader />}
+      {item.id ? <ItemDetail item={item} /> : <Loader />}
     </div>
   );
 }
