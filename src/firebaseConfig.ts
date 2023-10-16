706da3c97180739af3c2438fb56fa5e0702ca105
tamif <tamarafrazzetta@gmail.com>
2023-10-16 12:59:49 -0300

    fix: remove output from package json

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,19 +1,15 @@
 import { FirebaseOptions, initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-let firebaseConfig: FirebaseOptions = {
-    apiKey: JSON.stringify(process.env.API_KEY),
-    authDomain: JSON.stringify(process.env.AUTH_DOMAIN),
-    projectId: JSON.stringify(process.env.PROJECT_ID),
-    storageBucket: JSON.stringify(process.env.STORAGE_BUCKET),
-    messagingSenderId: JSON.stringify(process.env.MESSAGING_SENDER_ID),
-    appId: JSON.stringify(process.env.APP_ID),
-    measurementId: JSON.stringify(process.env.MEASUREMENT_ID)
-  };
-
-
-export default firebaseConfig;
-
+const firebaseConfig: FirebaseOptions = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
