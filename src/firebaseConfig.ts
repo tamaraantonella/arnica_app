7a9478686a40891780361c6365db130e4f275b86
tamif <tamarafrazzetta@gmail.com>
2023-10-16 12:17:25 -0300

    fix: firebaseConfig

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,20 +1,7 @@
 import { FirebaseOptions, initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-let firebaseConfig: FirebaseOptions;
-
-if (import.meta.env.MODE === 'development') {
-  firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID,
-    measurementId: import.meta.env.VITE_MEASUREMENT_ID
-  };
-} else {
-  firebaseConfig = {
+export const firebaseConfig: FirebaseOptions = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
     projectId: process.env.PROJECT_ID,
@@ -23,10 +10,6 @@ if (import.meta.env.MODE === 'development') {
     appId: process.env.APP_ID,
     measurementId: process.env.MEASUREMENT_ID
   };
-}
-
-export default firebaseConfig;
-
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
