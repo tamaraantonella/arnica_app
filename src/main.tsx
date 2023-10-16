3b59badbfe0ccfae1a8c019121fce24565972c35
tamif <tamarafrazzetta@gmail.com>
2023-10-16 10:30:18 -0300

    refactor: include ts, prettier and vite

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { CartProvider } from './context/CartContext';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <CartProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CartProvider>
+  </React.StrictMode>
+);
